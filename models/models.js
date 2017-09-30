3132499472b6b3d49bc119d793d8a1c450d1d721
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-09-29 21:24:04 -0600

    trying to get mongo db set up, adding mongoose schema/model

---
+++ models/models.js
@@ -0,0 +1,21 @@
+var Schema = mongoose.Schema;
+
+var ArticleSchema - new Schema({
+	title: {
+		type: String,
+		trim: true,
+		required: "Title is Required"
+	},
+
+	date: {
+		type: Date,
+	},
+
+	url: {
+		type: String
+	}
+});
+
+var Articles = mongoose.model("Articles", ArticleSchema);
+
+module.exports = Articles;
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var Articles = require("models/models.js")
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/nyt");
+
+var app = express();
+
+// Set the app up with morgan, body-parser, and a static folder
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
+
+// Database configuration
+var databaseUrl = "nytreactdb";
+var collections = ["article"];
+
+// Hook mongojs config to db variable
+var db = mongojs(databaseUrl, collections);
+
+// Log any mongojs errors to console
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
