9456f10045910419577d0ce0706c76c92dc196e1
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-09-29 22:33:43 -0600

    api routes working

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+apiRoutes(app);
+
 var port = process.env.PORT || 3000;
 
 mongoose.Promise = Promise;
